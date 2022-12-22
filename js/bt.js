1674cb3f9721a2ebd027bf18460fc3596f27d89e
kwonjun <jun00060007@gmail.com>
2022-12-22 10:49:16 +0900

    first commit

---
+++ js/bt.js
@@ -0,0 +1,5 @@
+// transform: translateY(-100vw)
+
+document.querySelector('.js_bt2').addEventListener('click', function(){
+  document.querySelector('.law_ul').style.transform = 'translateY(550)';
+});
